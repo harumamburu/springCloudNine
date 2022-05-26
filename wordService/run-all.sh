7cb31291194dfb0f876cb3993b9da8d66861744f
harumamburu <nikolay.kovalev92@gmail.com>
2022-05-26 18:02:23 +0300

    Add minor infra fixes

--- wordService/run-all.sh
+++ wordService/run-all.sh
@@ -1,12 +1,12 @@
-./gradlew bootRun --args='--sentence.part=subject' >./subject.log 2>&1 &
-./gradlew bootRun --args='--sentence.part=verb' >./verb.log 2>&1 &
-./gradlew bootRun --args='--sentence.part=article' >./article 2>&1 &
-./gradlew bootRun --args='--sentence.part=adjective' >./adjective.log 2>&1 &
-./gradlew bootRun --args='--sentence.part=noun' >./noun.log 2>&1 &
+./gradlew bootRun --args='--sentence.part=subject' >./logs/subject.log 2>&1 &
+./gradlew bootRun --args='--sentence.part=verb' >./logs/verb.log 2>&1 &
+./gradlew bootRun --args='--sentence.part=article.log' >./logs/article.log 2>&1 &
+./gradlew bootRun --args='--sentence.part=adjective' >./logs/adjective.log 2>&1 &
+./gradlew bootRun --args='--sentence.part=noun' >./logs/noun.log 2>&1 &
 
 if [[ "$1" == "addCold" ]]; then
-  ./gradlew bootRun --args='--sentence.part=cold-adjective --app.name.postfix=adjective' >./cold-adjective.log 2>&1 &
-  ./gradlew bootRun --args='--sentence.part=cold-noun --app.name.postfix=noun' >./cold-noun.log 2>&1 &
+  ./gradlew bootRun --args='--sentence.part=cold-adjective --app.name.postfix=adjective' >./logs/cold-adjective.log 2>&1 &
+  ./gradlew bootRun --args='--sentence.part=cold-noun --app.name.postfix=noun' >./logs/cold-noun.log 2>&1 &
 fi
 
 ps T
