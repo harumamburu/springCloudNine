ad448b3f3aae631df674c0fb27efd2b73df5f49e
harumamburu <nikolay.kovalev92@gmail.com>
2022-06-08 14:04:23 +0300

    Add docker files

--- eurekaServer/run-all.sh
+++
@@ -1,4 +0,0 @@
-./gradlew bootRun >./eureka-prime.log 2>&1 &
-./gradlew bootRun --args='--spring.profiles.active=secondary' >./eureka-second.log 2>&1 &
-./gradlew bootRun --args='--spring.profiles.active=tertiary' >./eureka-third.log 2>&1 &
-ps T
